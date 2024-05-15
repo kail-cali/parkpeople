1fb3e0739b58221af491f061d36123bd7d56448a
Hail-cali <willnotproud5@gmail.com>
2024-05-15 22:30:32 +0900

    Initial Commit: Kick Off Project ========================================================================

---
+++ src/main/java/co/parkpeople/ParkpeopleApplication.java
@@ -0,0 +1,13 @@
+package co.parkpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkpeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkpeopleApplication.class, args);
+    }
+
+}
