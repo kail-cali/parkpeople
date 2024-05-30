25205b32075bb8e10edd6061ff41eb325df5d9a3
Hail-cali <willnotproud5@gmail.com>
2024-05-30 14:52:19 +0900

    Rebase: change lang to Kotlin ==================================================================

---
+++ src/test/kotlin/co/parkpeople/ParkpeopleKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.parkpeople
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkpeopleKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
