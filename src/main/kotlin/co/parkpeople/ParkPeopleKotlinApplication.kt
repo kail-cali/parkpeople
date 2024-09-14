7e998659c94ff4684fe84b834362e2cf28c46df4
Hail-cali <willnotproud5@gmail.com>
2024-09-14 16:56:02 +0900

    [Server] rebase: change language kotlin

--- src/main/kotlin/co/parkpeople/ParkPeopleKotlinApplication.kt
+++ src/main/kotlin/co/parkpeople/ParkPeopleKotlinApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ParkpeopleKotlinApplication
+class ParkPeopleKotlinApplication
 
 fun main(args: Array<String>) {
-    runApplication<ParkpeopleKotlinApplication>(*args)
+    runApplication<ParkPeopleKotlinApplication>(*args)
 }
